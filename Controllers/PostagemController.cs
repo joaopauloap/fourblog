7ba750db944465b73b39bb1ac217e6ee46fda934
João Paulo de Araújo Porto <joaopaulo.porto@foursys.com.br>
2022-07-13 16:16:18 -0300

    tema bootswatch Journal adicionado, adicionada tag helpers para mensagem e erro, criado controllers tagController, postagemController

---
+++ Controllers/PostagemController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FourBlog.Controllers
+{
+    public class PostagemController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        public IActionResult Visualizar()
+        {
+
+            return View();
+        }
+    }
+}
---
+++ Controllers/TagController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FourBlog.Controllers
+{
+    public class TagController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Editar()
+        {
+            return View();
+        }
+    }
+}
